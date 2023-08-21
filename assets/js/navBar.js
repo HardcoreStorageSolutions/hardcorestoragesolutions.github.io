6c694b34c39fecf07df5589422ce44eddb27beb5
Mary Prince <mleeherr@gmail.com>
2023-08-21 15:37:07 -0400

    append is only allowing me to add 1 span - it's so weird - maybe it needs to be a list

--- assets/js/navBar.js
+++ assets/js/navBar.js
@@ -2,7 +2,34 @@
 
 console.log("Hello world!");
 
-var navBar = document.getElementById("myNavbar");
 
-navBar.innerHTML="<div class=container><nav class=green>this is a test<button>button</button></nav></div>";
+
+let navbarContainer = document.getElementById("navBarContainer");
+
+function homeLink(){
+    let link = document.createElement('a');
+    let homeLink= document.createTextNode("Home");
+    link.appendChild(homeLink);
+    link.href="https://www.hardcorestorage.com";
+    navbarContainer.className="container";
+    navbarContainer.append(link);
+
+    let button= document.createElement('button');
+    button.className="navbar-toggle";
+    button.setAttribute("data-toggle","collapse");
+    button.setAttribute("data-target","#myNavbar");
+    
+    let span=document.createElement('span');
+    span.setAttribute("class","icon-bar");
+    navbarContainer.append(button);
+    // for (let i=0; i < 3; i++){button.append(span);}
+    button.append(span, "another thing");
+    button.append(span);
+
+    console.log(button.innerHTML);
+      
+    
+}
+
+homeLink();
 
