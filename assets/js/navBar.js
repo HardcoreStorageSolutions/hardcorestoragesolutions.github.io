bd3de87bc2f6b98e74255248e18806fae0408867
Mary Prince <mleeherr@gmail.com>
2023-08-23 13:04:45 -0400

    progressin'! with that for loop beautifuls

--- assets/js/navBar.js
+++ assets/js/navBar.js
@@ -15,11 +15,28 @@ hButton.append(hamburger);
 
 outerNav.append(hButton);
 
+let navLinks=['Home', 'about'];
+for (let i=0; i < navLinks.length; i++){
+    let navLink=document.createElement('a');
+    let navLinkText=document.createTextNode(navLinks[i]);
+    navLink.appendChild(navLinkText);
+    navLink.href="https://www.hardcorestorage.com";
+    navLink.className="btn btn=primary";
+    innerNav.append(navLink);
+    console.log(navLinks[i]);
+}
 
 // --save this for formatting--//
-let link = document.createElement('button');
-let homeLink= document.createTextNode("Home");
-link.appendChild(homeLink);
-link.href="https://www.hardcorestorage.com";
-link.className="btn btn-primary";
-innerNav.append(link);
+// let link = document.createElement('a');
+// let homeLink= document.createTextNode("Home");
+// link.appendChild(homeLink);
+// link.href="https://www.hardcorestorage.com";
+// link.className="btn btn-primary";
+// innerNav.append(link);
+
+// let link2 = document.createElement('a');
+// let aboutlink = document.createTextNode('About');
+// link2.appendChild(aboutlink);
+// link2.href="https://www.hardcorestorage.com/pages/about.html";
+// link2.className="btn btn-primary";
+// innerNav.append(link2);
