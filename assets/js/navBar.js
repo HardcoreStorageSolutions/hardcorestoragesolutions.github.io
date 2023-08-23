229569339c4762d0bf6dc2c4faf6744eaff4f185
Mary Prince <mleeherr@gmail.com>
2023-08-23 13:13:42 -0400

    the for loop works for links on the navbar, which all disappear when the window is shrunk and the hamburger icon appears

--- assets/js/navBar.js
+++ assets/js/navBar.js
@@ -15,28 +15,16 @@ hButton.append(hamburger);
 
 outerNav.append(hButton);
 
-let navLinks=['Home', 'about'];
+//these are the nav links - the two arrays will need to match to where we want things to go.
+let navLinks=['Home', 'About', 'Contact', 'Order CanMod'];
+let navLinksHttp=['https://www.hardcorestorage.com', 'https://www.hardcorestorage.com/pages/about.html', 'https://shop.hardcorestorage.com/pages/contact', 'https://shop.hardcorestorage.com/'];
 for (let i=0; i < navLinks.length; i++){
     let navLink=document.createElement('a');
     let navLinkText=document.createTextNode(navLinks[i]);
     navLink.appendChild(navLinkText);
-    navLink.href="https://www.hardcorestorage.com";
-    navLink.className="btn btn=primary";
+    navLink.href=navLinksHttp[i];
+    navLink.className="btn btn-primary";
     innerNav.append(navLink);
     console.log(navLinks[i]);
 }
 
-// --save this for formatting--//
-// let link = document.createElement('a');
-// let homeLink= document.createTextNode("Home");
-// link.appendChild(homeLink);
-// link.href="https://www.hardcorestorage.com";
-// link.className="btn btn-primary";
-// innerNav.append(link);
-
-// let link2 = document.createElement('a');
-// let aboutlink = document.createTextNode('About');
-// link2.appendChild(aboutlink);
-// link2.href="https://www.hardcorestorage.com/pages/about.html";
-// link2.className="btn btn-primary";
-// innerNav.append(link2);
