da48b85af8ae9d682524a44d4d5cd0f39e998524
Mary Prince <mleeherr@gmail.com>
2023-08-23 12:33:35 -0400

    using the docs from bootstrap, i figured out that there are 3 containers; the container that holds the navbar that holda a div.  the div is where the links that appear when everything is expanded, and the navbar is where links/buttons appear when on a smaller screen.  the container on the outside is just holding it all and allowing some basic formatting on the page.

---
+++ assets/js/navbar2.js
@@ -0,0 +1,28 @@
+console.log("Hello World!");
+
+let navbar = document.getElementById("myNavbarID");
+myNavbarID.className=("green");
+
+
+let innerNav=document.getElementById("navBarContainer");
+innerNav.className="collapse navbar-collapse";
+
+let waffle=document.createElement('button');
+waffle.className=("btn-primary navbar-toggle nav-item");
+waffle.innerHTML="Here is a Waffle";
+
+let waffle2=document.createElement('button');
+waffle2.className=("btn-primary navbar-brand");
+waffle2.innerHTML="Here is another Waffle";
+
+
+navbar.append(waffle);
+innerNav.append(waffle2);
+
+
+//the above allows for the pretty button to appear when screen is minimized, but disappears when maximized.
+//the things that matter are, that the navbar is a navbar which has special properties.  If there is something inside it, it won't collapse.  
+//navbar toggle makes the navbar appear when the screen in minized if that item has the navbar-toggle class.  if it does not have the class and it's inside the navbar, the navbar will stay expanded
+//if I want to have the navbar show items when expanded - how do i do that? lets try adding another button - added second one with out navbar toggle let's try navbarbrand
+
+//UPDATE!!! I DID IT!!!
